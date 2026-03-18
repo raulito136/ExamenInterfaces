ddd5c9f2244fb6941f26a74a4c63b63ee690fc68
RaúlLópezPalomo <raulitolopez136@gmail.com>
2026-03-18 07:56:55 +0100

    Preparacion Examen

---
+++ src/main/java/org/example/exameninterfaces/ExamenInterfacesApplication.java
@@ -0,0 +1,13 @@
+package org.example.exameninterfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenInterfacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenInterfacesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/exameninterfaces/ExamenInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exameninterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenInterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
